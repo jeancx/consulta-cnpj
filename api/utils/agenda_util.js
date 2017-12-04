85bedbba59da3958e0c39dcf1e515be0a9bed807
jeancx <contato@jeancf.com.br>
2017-12-03 23:00:53 -0200

    Agenda

---
+++ api/utils/agenda_util.js
@@ -0,0 +1,17 @@
+const mongo_url = process.env.MONGO_URL || 'mongodb://localhost/consulta_cnpj';
+
+//https://github.com/agenda/agenda
+var agenda = new Agenda({db: {address: mongo_url}});
+
+agenda.define('delete old users', function (job, done) {
+    User.remove({lastLogIn: {$lt: twoDaysAgo}}, done);
+});
+
+agenda.on('ready', function () {
+    agenda.every('3 minutes', 'delete old users');
+
+    // Alternatively, you could also do:
+    agenda.every('*/3 * * * *', 'delete old users');
+
+    agenda.start();
+});
