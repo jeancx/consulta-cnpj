1b5a06b0f8f18560ad82d76a4907ee2370961d0f
jeancx <contato@jeancf.com.br>
2017-12-03 23:12:40 -0200

    descrição da rotina

--- api/controllers/consulta_cnpj.js
+++ api/controllers/consulta_cnpj.js
@@ -11,17 +11,19 @@ const Empresa = require('../models/empresa');
 
 
 // get all
-exports.getCNPJ = function(req, res, next) {
+exports.getCNPJ = function (req, res, next) {
 
     const query = {
         cnpj: req.params.cnpj
     };
-
+    //encontra ou Cria
     Empresa.findOrCreate(query, function (err, model) {
+        //se tiver data traz o resultado
         if (model.ultima_atualizacao !== null) {
             res.json(model);
-        } else {
-            //anticaptcha_util.get
+        } else { //se não tiver data pega os dados atulizados com Crawler / Anticaptcha
+            //pega os dados numa thread e atualiza no banco
+            //retorna os dados do receitaws para rápida resposta
         }
     });
 
